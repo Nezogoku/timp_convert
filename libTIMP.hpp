a7b5325063b80afad984259313c90647dcdbadac
Nezogoku <97012270+Nezogoku@users.noreply.github.com>
2022-07-11 15:14:00 -0400

    Update libTIMP.hpp
    Added additional functions

--- libTIMP.hpp
+++ libTIMP.hpp
@@ -1,22 +1,44 @@
 #ifndef LIBTIMP_HPP
 #define LIBTIMP_HPP
 
-#include <iostream>
-#include <SFML/Graphics.hpp>
 #include <fstream>
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <SFML/Graphics.hpp>
+#include <string>
+#include <vector>
 
-class libTIMP
-{
+class timp {
     public:
-    uint16_t width, height;
-    int chunk_w,chunk_h;
+        timp();
+        timp(bool isDebug);
 
-    std::vector<int> pixels;
-    std::vector<sf::Color> pal_color;
+        void loadFile(std::string file);
+        int setPixels();
 
-    libTIMP();
-    void load(std::string file);
-    sf::Image convertToImage();
+        int getImgWidth();
+        int getImgHeight();
+        std::string getImgName();
+
+        void displayImage();
+        void debugImage();
+        int convertImage(std::string outfile);
+
+    private:
+        sf::View letterBox(sf::View view, int windX, int windY);
+        sf::Image image;
+        sf::Image imgchunk;
+
+        uint16_t width = 0, height = 0;
+        int chunk_w = 0,chunk_h = 0;
+        std::string fileName = "";
+
+        bool debug = false;
+        bool noPal;
+
+        std::vector<sf::Color> pal_color;
+        std::vector<uint8_t> pixels;
 };
 
-#endif // LIBTIMP_HPP
+#endif
