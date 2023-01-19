4bfab162ca4e2ef83e32ebf33ce297a58e8ed966
Cheena <97012270+Nezogoku@users.noreply.github.com>
2023-01-19 15:16:31 -0500

    Update toCompressed.cpp
    Changed all transparent backgrounds to solid white for JPG conversion

--- convert/toCompressed.cpp
+++ convert/toCompressed.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <string>
+#include <vector>
 #include "convert.hpp"
 #include "png/lodepng.h"
 #include "jpg/toojpeg.h"
@@ -9,7 +10,10 @@
 int outPNG(std::string dst_file, const unsigned char* src, int width, int height) {
     dst_file = dst_file.substr(0, dst_file.find_last_of('.')) + ".png";
 
-    return !(lodepng::encode(dst_file, src, width, height, LCT_RGBA, 8));
+    std::vector<unsigned char> png;
+    auto error = lodepng::encode(dst_file, src, width, height, LCT_RGBA, 8);
+
+    return !error;
 }
 
 
@@ -18,9 +22,15 @@ int outJPG(std::string dst_file, const unsigned char* src, int width, int height
     unsigned char src24[width * height * 3] = {};
 
     for (int p = 0, s = 0; p < width * height * 4; ++p) {
-        src24[s++] = src[p++];
-        src24[s++] = src[p++];
-        src24[s++] = src[p++];
+        unsigned char charR, charG, charB;
+        charR = src[p++];
+        charG = src[p++];
+        charB = src[p++];
+
+        if (src[p] < (0xFF * 0.75)) charR = charG = charB = 0xFF;
+        src24[s++] = charR;
+        src24[s++] = charG;
+        src24[s++] = charB;
     }
 
     dst_file = dst_file.substr(0, dst_file.find_last_of('.')) + ".jpg";
@@ -29,7 +39,7 @@ int outJPG(std::string dst_file, const unsigned char* src, int width, int height
     const bool isRGB = true;
     const int quality = 100;
     const bool downsample = false;
-    const char* comment = "Converted from TIMP image using convertTIMP";
+    const char* comment = "Converted from TIMP image";
 
     auto toDst = [](unsigned char byt)->void{ out.put(byt); };
     auto isSuccess = TooJpeg::writeJpeg(toDst, src24, width, height,
